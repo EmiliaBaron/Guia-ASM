408c839d631674cc4a048ed76352a061c59a55d0
Emilia Sofía Baron <emiliasbaron@gmail.com>
2025-09-10 12:01:14 -0300

    agregaciones

--- GuiaASM/src/3 - Alineación y Estructuras/Estructuras.asm
+++ GuiaASM/src/3 - Alineación y Estructuras/Estructuras.asm
@@ -2,19 +2,19 @@
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_NEXT EQU 0
 NODO_OFFSET_CATEGORIA EQU 8
-NODO_OFFSET_ARREGLO EQU 8
-NODO_OFFSET_LONGITUD EQU 8
-NODO_SIZE EQU 8
-PACKED_NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_ARREGLO EQU 16
+NODO_OFFSET_LONGITUD EQU 24
+NODO_SIZE EQU 32
+PACKED_NODO_OFFSET_NEXT EQU 0
 PACKED_NODO_OFFSET_CATEGORIA EQU 8
-PACKED_NODO_OFFSET_ARREGLO EQU 8
-PACKED_NODO_OFFSET_LONGITUD EQU 8
-PACKED_NODO_SIZE EQU 8
-LISTA_OFFSET_HEAD EQU 8
+PACKED_NODO_OFFSET_ARREGLO EQU 16
+PACKED_NODO_OFFSET_LONGITUD EQU 24
+PACKED_NODO_SIZE EQU 28
+LISTA_OFFSET_HEAD EQU 0
 LISTA_SIZE EQU 8
-PACKED_LISTA_OFFSET_HEAD EQU 8
+PACKED_LISTA_OFFSET_HEAD EQU 0
 PACKED_LISTA_SIZE EQU 8
 
 ;########### SECCION DE DATOS
@@ -31,6 +31,19 @@ global cantidad_total_de_elementos_packed
 ;extern uint32_t cantidad_total_de_elementos(lista_t* lista);
 ;registros: lista[rdi]
 cantidad_total_de_elementos:
+	push RBP ;pila alineada
+  	mov RBP, RSP ;strack frame armado
+
+	XOR eax, eax
+	.ciclo:
+    	mov rsi, [RDI + eax * 8 + 0 ]
+
+		INC eax ;avanzo el índice del array
+		xor r8d
+    	CMP R8, RSI
+    	JL .ciclo
+
+
 	ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
